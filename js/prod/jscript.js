65072c76215550536a9a26d920d3e2fe0916bb7c
Laurynas B <l.butrimavicius@gmail.com>
2018-03-19 17:23:21 +0200

    commit

--- js/prod/jscript.js
+++ js/prod/jscript.js
@@ -23,64 +23,7 @@ window.onclick = function(event) {
     }
 };
 
-addTo.addEventListener("click", function() {
-    required();
-});
 
-function required()
-{
-    var addIn = document.getElementById("forma");
-    var error = '';
-    var i;
-    for (i = 0; i < addIn.elements.length; i++) {
-        if (addIn.elements[i].value === ""){
-            error += 'Neužpildytas laukelis: ' + addIn.elements[i].getAttribute('name') + '\n';
-        }
-    }
-
-    if(error != ''){
-        alert(error);
-        return
-    }
-
-    reverse();
-}
-
-function reverse() {
-
-    var addIn = document.getElementById("forma");
-    var list = document.querySelector(".person .row");
-    var i;
-
-    var node = document.createElement('div');
-    node.classList.add('col-md-6');
-    node.appendChild(document.createTextNode(addIn.elements[0].getAttribute('name') + ' ' + addIn.elements[1].getAttribute('name')));
-    list.appendChild(node);
-
-    var form = document.createElement('div');
-    form.classList.add('col-md-6');
-    form.appendChild(document.createTextNode((addIn.elements[0].value) + ' ' + (addIn.elements[1].value)));
-    list.appendChild(form);
-
-
-    for (i = 0; i < addIn.elements.length; i++) {
-        if (i >= 2) {
-
-            var node = document.createElement('div');
-            node.classList.add('col-md-6');
-            node.appendChild(document.createTextNode(addIn.elements[i].getAttribute('name')));
-            list.appendChild(node);
-
-            var form = document.createElement('div');
-            form.classList.add('col-md-6');
-            form.appendChild(document.createTextNode(addIn.elements[i].value));
-            list.appendChild(form);
-        }
-    }
-
-    modal.style.display = "none";
-    body.style.overflow = "scroll";
-}
 ;(function (window, $, undefined) { (function () {
     var VERSION = '2.2.3',
         pluginName = 'datepicker',
