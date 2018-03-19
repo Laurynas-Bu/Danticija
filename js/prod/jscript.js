0a9d9339181f4b904123432c67c9d5a13cfe818a
Laurynas <l.butrimavicius@gmail.com>
2018-03-20 00:27:22 +0200

    Dev

--- js/prod/jscript.js
+++ js/prod/jscript.js
@@ -1,29 +1,56 @@
-var modal, span, body, addTo;
+$(document).ready(function () {
 
-modal = document.getElementById('div1');
-span = document.getElementsByClassName("close")[0];
-body = document.querySelector('body');
-addTo = document.getElementById('addto');
+    var modal, span, body, addTo;
 
-$('#regbtn1, #regbtn2').click(function() {
-    modal.style.display = "block";
-    body.style.overflow = "hidden";
+    modal = document.getElementById('div1');
+    span = document.getElementsByClassName("close")[0];
+    body = document.querySelector('body');
+    addTo = document.getElementById('addto');
 
-});
+    $('#regbtn1, #regbtn2').click(function () {
+        modal.style.display = "block";
+        body.style.overflow = "hidden";
 
-span.onclick = function() {
-    modal.style.display = "none";
-    body.style.overflow = "scroll";
-};
+    });
 
-window.onclick = function(event) {
-    if (event.target == modal) {
+    span.onclick = function () {
         modal.style.display = "none";
         body.style.overflow = "scroll";
-    }
-};
+    };
 
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            body.style.overflow = "scroll";
+        }
+    };
 
+    $('.accordionSpec > ul > li:has(ul)').addClass("has-sub");
+
+    $('.accordionSpec > ul > li > a').click(function () {
+
+        var checkElement = $(this).next();
+
+        $('.accordionSpec li').removeClass('active');
+        $(this).closest('li').addClass('active');
+
+        if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+            $(this).closest('li').removeClass('active');
+            checkElement.slideUp('normal');
+        }
+
+        if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+            $('.accordionSpec ul ul:visible').slideUp('normal');
+            checkElement.slideDown('normal');
+        }
+
+        if (checkElement.is('ul')) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+});
 ;(function (window, $, undefined) { (function () {
     var VERSION = '2.2.3',
         pluginName = 'datepicker',
